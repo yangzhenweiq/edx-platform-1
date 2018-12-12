6626decb4e457c9bae83917511d5fff606a7496b
sodaling <502464760@qq.com>
2018-12-12 10:23:38 +0800

    modified by front suggestion

--- openedx/core/djangoapps/user_api/urls.py
+++ openedx/core/djangoapps/user_api/urls.py
@@ -70,8 +70,10 @@ RETIREMENT_LMS_POST = LMSAccountRetirementView.as_view({
     'post': 'post',
 })
 
-SEND_AND_VERIFY_CODE_FOR_BINDING = PhoneBindingViewSet.as_view({
-    'get': 'get',
+SEND_CODE_FOR_BINDING = PhoneBindingViewSet.as_view({
+    'post': 'send',
+})
+VERIFY_CODE_FOR_BINDING = PhoneBindingViewSet.as_view({
     'post': 'post',
 })
 
@@ -172,8 +174,13 @@ urlpatterns = [
         name='preferences_detail_api'
     ),
     url(
+        r'^v1/accounts/send_code_binding_phone/$',
+        SEND_CODE_FOR_BINDING,
+        name='send_code_binding_phone'
+    ),
+    url(
         r'^v1/accounts/binding_phone/$',
-        SEND_AND_VERIFY_CODE_FOR_BINDING,
+        VERIFY_CODE_FOR_BINDING,
         name='binding_phone'
     ),
 ]
