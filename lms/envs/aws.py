2d5a37eec9e8665c7176ddd9e4581582ee497ff9
sodaling <502464760@qq.com>
2019-05-07 16:59:16 +0800

    finish aws setting

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -26,8 +26,7 @@ import dateutil
 from path import Path as path
 from xmodule.modulestore.modulestore_settings import convert_module_store_setting_if_needed
 
-from .common import *
-from .eliteu import *
+from .eliteu_common import *
 
 from openedx.core.lib.derived import derive_settings  # pylint: disable=wrong-import-order
 from openedx.core.lib.logsettings import get_logger_config  # pylint: disable=wrong-import-order
@@ -1113,41 +1112,3 @@ plugin_settings.add_plugins(__name__, plugin_constants.ProjectType.LMS, plugin_c
 derive_settings(__name__)
 
 logging.warn('DEPRECATION WARNING: aws.py has been deprecated, you should use production.py instead.')
-
-ALIPAY_INFO = AUTH_TOKENS.get('ALIPAY_INFO', ALIPAY_INFO)
-ALIPAY_APP_INFO = AUTH_TOKENS.get('ALIPAY_APP_INFO', ALIPAY_APP_INFO)
-WECHAT_PAY_INFO = AUTH_TOKENS.get('WECHAT_PAY_INFO', WECHAT_PAY_INFO)
-WECHAT_APP_PAY_INFO = AUTH_TOKENS.get('WECHAT_APP_PAY_INFO', WECHAT_APP_PAY_INFO)
-WECHAT_H5_PAY_INFO = AUTH_TOKENS.get('WECHAT_H5_PAY_INFO', WECHAT_H5_PAY_INFO)
-
-if FEATURES.get('ENABLE_MEMBERSHIP_INTEGRATION', False):
-    INSTALLED_APPS.append('membership')
-    REST_FRAMEWORK.update({'EXCEPTION_HANDLER': 'membership.utils.customer_exception_handler'})
-
-############## Settings for python-social-auth ######################
-SOCIAL_AUTH_TRAILING_SLASH = ENV_TOKENS.get('SOCIAL_AUTH_TRAILING_SLASH', False)
-
-# Aliyun oss
-ALIYUN_OSS = AUTH_TOKENS.get('ALIYUN_OSS', {})
-
-OSS_ACCESS_KEY_ID = ALIYUN_OSS.get("OSS_ACCESS_KEY_ID", "")
-OSS_ACCESS_KEY_SECRET = ALIYUN_OSS.get("OSS_ACCESS_KEY_SECRET", "")
-OSS_ENDPOINT = ALIYUN_OSS.get("OSS_ENDPOINT", "")
-OSS_BUCKET_NAME = ALIYUN_OSS.get("OSS_BUCKET_NAME", "")
-
-#for SMS
-SMS_API = AUTH_TOKENS.get('SMS_API','')
-SMS_API_BY_LINKGROUP = AUTH_TOKENS.get('SMS_API_BY_LINKGROUP','')
-SMS_API_URL = ENV_TOKENS.get('SMS_API_URL','')
-SMS_API_URL_BY_LINKGROUP = ENV_TOKENS.get('SMS_API_URL_BY_LINKGROUP','')
-
-######################## Professors ###########################
-
-if FEATURES.get('ENABLE_PROFESSORS'):
-    INSTALLED_APPS.append('professors')
-
-############################ WEIXINAPPID_AND_WEIXINAPPSECRET #########################
-WEIXINAPPID = ''
-WEIXINAPPSECRET = ''
-WEIXINAPPID = ENV_TOKENS.get('WEIXINAPPID', WEIXINAPPID)
-WEIXINAPPSECRET = ENV_TOKENS.get('WEIXINAPPSECRET', WEIXINAPPSECRET)
