3b13619e5cce482f4619167531ad32e7a808e44f
zyingli <zyingli0007@gmail.com>
2019-06-05 16:50:12 +0800

    edx payments plugin

--- lms/envs/eliteu_common.py
+++ lms/envs/eliteu_common.py
@@ -34,10 +34,10 @@ FEATURES.update({
 })
 
 MEMBERSHIP_ROOT = REPO_ROOT / "../edx-membership"
-PAYMENTS_ROOT = REPO_ROOT / "../eliteu-payments"
+# PAYMENTS_ROOT = REPO_ROOT / "../eliteu-payments"
 
 sys.path.append(MEMBERSHIP_ROOT)
-sys.path.append(PAYMENTS_ROOT)
+# sys.path.append(PAYMENTS_ROOT)
 
 OAUTH2_PROVIDER.update({
     'ACCESS_TOKEN_EXPIRE_SECONDS': 60 * 60 * 24 * 365,
@@ -235,105 +235,6 @@ COUNTRIES_OVERRIDE = {
     # Taiwan is specifically not translated to avoid it being translated as "Taiwan (Province of China)"
     # "TW": "Taiwan",
     'XK': _('Kosovo'),
-}
-
-############################ WECHAT_APP_PAY #########################
-WECHAT_APP_PAY_INFO = {
-    "basic_info": {
-        "APPID": "",
-        "APPSECRET": "",
-        "MCHID": "",
-        "KEY": "",
-        "ACCESS_TOKEN": ""
-    },
-    "other_info": {
-        "BUY_COURSES_SUCCESS_TEMPLATE_ID": "",
-        "BUY_COURSES_SUCCESS_HREF_URL": "",
-        "COIN_SUCCESS_TEMPLATE_ID": "",
-        "COIN_SUCCESS_HREF_URL": "",
-        "SERVICE_TEL": "",
-        "NOTIFY_URL": "",
-        "JS_API_CALL_URL": "",
-        "SSLCERT_PATH": "",
-        "SSLKEY_PATH": ""
-    }
-}
-
-############################ WECHAT_PAY #########################
-WECHAT_PAY_INFO = {
-    "basic_info": {
-        "APPID": "",
-        "APPSECRET": "",
-        "MCHID": "",
-        "KEY": "",
-        "ACCESS_TOKEN": ""
-    },
-    "other_info": {
-        "BUY_COURSES_SUCCESS_TEMPLATE_ID": "",
-        "BUY_COURSES_SUCCESS_HREF_URL": "",
-        "COIN_SUCCESS_TEMPLATE_ID": "",
-        "COIN_SUCCESS_HREF_URL": "",
-        "SERVICE_TEL": "",
-        "NOTIFY_URL": "",
-        "JS_API_CALL_URL": "",
-        "SSLCERT_PATH": "",
-        "SSLKEY_PATH": ""
-    }
-}
-
-############################ WECHAT H5 PAY #########################
-WECHAT_H5_PAY_INFO = {
-    "basic_info": {
-        "APPID": "",
-        "APPSECRET": "",
-        "MCHID": "",
-        "KEY": "",
-        "ACCESS_TOKEN": ""
-    },
-    "other_info": {
-        "SERVICE_TEL": "",
-        "NOTIFY_URL": "",
-        "JS_API_CALL_URL": "",
-        "SSLCERT_PATH": "",
-        "SSLKEY_PATH": "",
-        "SPBILL_CREATE_IP": ""
-    }
-}
-
-############################ ALIPAY_INFO #########################
-ALIPAY_APP_INFO = {
-    "basic_info": {
-        "APP_ID": "",
-        "APP_PRIVATE_KEY": "",
-        "ALIPAY_RSA_PUBLIC_KEY": ""
-    },
-    "other_info": {
-        "SIGN_TYPE": "",
-        "NOTIFY_URL": ""
-    }
-}
-
-############################ ALIPAY_INFO #########################
-ALIPAY_INFO = {
-    'basic_info': {
-        "KEY": "",
-        "PARTNER": "",
-        "SELLER_EMAIL": ""
-    },
-    'other_info': {
-        "INPUT_CHARSET": "",
-        "INPUT_DIRECT_CHARSET": "",
-        "SIGN_TYPE": "",
-        "RETURN_URL": "",
-        "NOTIFY_URL": "",
-        "REFUND_NOTIFY_URL": "",
-        "SHOW_URL": "",
-        "ERROR_NOTIFY_URL": "",
-        "TRANSPORT": "",
-        "DEFAULT_BANK": "",
-        "IT_B_PAY": "",
-        "REFUND_URL": ""
-    }
 }
 
 ACCOUNT_VISIBILITY_CONFIGURATION = {
