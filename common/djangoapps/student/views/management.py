79d07b7c8e4adfddd700393546c7a8652f8cbd34
xavierchan <xavierchan@163.com>
2020-02-18 02:26:59 +0800

    feat(hmm): create hmm account by activate
    create hmm account by activate

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -96,6 +96,7 @@ from util.bad_request_rate_limiter import BadRequestRateLimiter
 from util.db import outer_atomic
 from util.json_request import JsonResponse
 from util.password_policy_validators import normalize_password, validate_password
+import elitemba
 
 log = logging.getLogger("edx.student")
 
@@ -657,7 +658,10 @@ def activate_account(request, key):
         relativedelta(days=+int(settings.EMAIL_ACCESS_DATE))
         vip_info = VIPInfo.objects.filter(user=registration.user)
         if not vip_info:
-            VIPInfo.objects.create(user=registration.user, expired_at=expired_at)        
+            VIPInfo.objects.create(user=registration.user, expired_at=expired_at)
+
+        if registration.user.email.split('@')[-1] in settings.HMM_CONFIGS.get('ACCESS_LIST', {}).keys():
+            elitemba.create_by_activate(registration.user)
                
     return redirect('dashboard')
 
